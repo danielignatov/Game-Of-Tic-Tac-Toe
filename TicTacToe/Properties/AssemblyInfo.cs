f62a01f2aa7dde5314aa021eb21b8e870c632c8b
Daniel Ignatov <sa3ana3a@abv.bg>
2016-05-01 18:04:44 +0300

    Assembly Info Edit

--- TicTacToe/Properties/AssemblyInfo.cs
+++ TicTacToe/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicTacToe")]
+[assembly: AssemblyTitle("Game Of Tic Tac Toe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicTacToe")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Game Of Tic Tac Toe")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
